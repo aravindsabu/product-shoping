8d1027866e9ad659c5e7881ec1797d4d4bbab515
aravindsabu <aravindsabu2001@gmail.com>
2022-10-12 08:08:09 +0530

    ok

---
+++ src/app/allproduct/allproduct.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-allproduct',
+  templateUrl: './allproduct.component.html',
+  styleUrls: ['./allproduct.component.css']
+})
+export class AllproductComponent implements OnInit {
+
+  constructor() { }
+  product={}
+
+  ngOnInit(): void {
+  }
+
+}
